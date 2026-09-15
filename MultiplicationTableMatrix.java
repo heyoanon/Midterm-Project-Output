2e52e8efc861cc042c5c77710e1c7b7caca9f309
Dan <anonymousf4nx@gmail.com>
2026-09-15 16:42:12 +0800

    Create MultiplicationTableMatrix.java
    Initialize MultiplicationTableMatrix class with main method.

---
+++ MultiplicationTableMatrix.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MultiplicationTableMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//set parameters and code to run the program.
+
+  }
+}
