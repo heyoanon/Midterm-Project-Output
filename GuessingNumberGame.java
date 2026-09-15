7d1b92ca22fd1759abe3e4db1887d7f8f4151431
Dan <anonymousf4nx@gmail.com>
2026-09-15 16:45:06 +0800

    Create GuessingNumberGame class with main method
    Initialize GuessingNumberGame with imports and main method.

---
+++ GuessingNumberGame.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingNumberGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+//set parameters and code to run the program.
+
+  }
+}
